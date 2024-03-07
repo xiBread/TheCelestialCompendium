12ae3ff71ca106bf8d252f80bd5ebd4bc6071d88
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2024-03-07 18:33:31 -0500

    work on profiles

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,39 +3,43 @@ import { defineCollection, z } from "astro:content";
 export const collections = {
 	archive: defineCollection({
 		type: "content",
-		schema: z.object({
-			title: z.string(),
-			description: z.string().optional(),
-			meta: z
-				.object({
-					index: z.boolean().default(false),
-					order: z.number().default(Infinity),
-				})
-				.default({}),
-			/**
-			 * byr -> birth year
-			 * hgt -> height
-			 * wgt -> weight
-			 * hcl -> hair color
-			 * ecl -> eye color
-			 */
-			profile: z
-				.object({
-					aliases: z.array(z.string()).optional(),
-					sex: z.enum(["M", "F"]).optional(),
-					age: z.number().optional(),
-					byr: z.string().optional(),
-					hgt: z
-						.string()
-						.regex(/\d\d?'(?:\d\d?")?/)
-						.optional(),
-					wgt: z.string().optional(),
-					hcl: z.string().optional(),
-					ecl: z.string().optional(),
-				})
-				.optional(),
-		}),
+		schema: ({ image }) =>
+			z.object({
+				title: z.string(),
+				description: z.string().optional(),
+				meta: z
+					.object({
+						index: z.boolean().default(false),
+						order: z.number().default(Infinity),
+					})
+					.default({}),
+				/**
+				 * byr -> birth year
+				 * hgt -> height
+				 * wgt -> weight
+				 * hcl -> hair color
+				 * ecl -> eye color
+				 */
+				profile: z
+					.object({
+						portrait: image().optional(),
+						title: z.string().optional(),
+						aliases: z.array(z.string()).default([]),
+						sex: z.enum(["M", "F"]).optional(),
+						age: z.number().optional(),
+						byr: z.string().optional(),
+						hgt: z
+							.string()
+							.regex(/\d\d?'(?:\d\d?")?/)
+							.optional(),
+						wgt: z.number().optional(),
+						hcl: z.string().optional(),
+						ecl: z.string().optional(),
+					})
+					.optional(),
+			}),
 	}),
+	// For timeline.yml only
 	data: defineCollection({
 		type: "data",
 		schema: ({ image }) =>
